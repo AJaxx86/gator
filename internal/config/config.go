1e6a4e8687d8b4ccb499d985b7b81fc8d7a892c7
AJaxx86 <62261949+AJaxx86@users.noreply.github.com>
2026-07-03 22:32:53 +0100

    added config

---
+++ internal/config/config.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"os"
+	"encoding/json"
+	"fmt"
+)
+
+const configFileName string = ".gatorconfig.json"
+
+type Config struct {
+	DBURL string
+	UserName string
+}
+
+
+func Read() (Config, error) {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	file, err := os.Open(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	cfg := Config{}
+	jErr := json.NewDecoder(file).Decode(&cfg)
+	if jErr != nil {
+		return Config{}, jErr
+	}
+
+	return cfg, nil
+}
+
+
+func (cfg Config) SetUser(name string) error {
+	cfg.UserName = name
+	if err := write(cfg); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s/%s", homeDir, configFileName), nil
+}
+
+
+func write(cfg Config) error {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	wErr := os.WriteFile(filePath, data, 0644)
+	if wErr != nil {
+		return wErr
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"github.com/ajaxx86/gator/internal/config"
+	"fmt"
+)
+
 func main() {
-	// shit goes here
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println("Error reading config:", err)
+		return
+	}
+	fmt.Println("Config:", cfg)
+
+	cfg.SetUser("jaxx")
+	cfg, err = config.Read()
+	if err != nil {
+		fmt.Println("Error reading config:", err)
+		return
+	}
+	fmt.Println("Updated config:", cfg)
 }
